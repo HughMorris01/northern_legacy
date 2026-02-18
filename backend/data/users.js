90916166f0a08d4bfe4ae8b01a9a92e552eb0af7
Greg Farrell <gfarrell2@Live.com>
2026-02-17 22:20:57 -0500

    Update seed data

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,7 +5,7 @@ const users = [
     firstName: 'Admin',
     lastName: 'NorthernLegacy',
     email: 'admin@northernlegacy.com',
-    password: bcrypt.hashSync('123456', 10),
+    passwordHash: bcrypt.hashSync('123456', 10),
     role: 'admin',
     isVerified: true,
     idDocumentHash: 'ADMIN-HASH-001'
@@ -14,7 +14,7 @@ const users = [
     firstName: 'Greg',
     lastName: 'Customer',
     email: 'greg@aol.com',
-    password: bcrypt.hashSync('123', 10),
+    passwordHash: bcrypt.hashSync('123', 10),
     role: 'customer',
     isVerified: true,
     idDocumentHash: 'GREG-HASH-001'
@@ -23,7 +23,7 @@ const users = [
     firstName: 'Mike',
     lastName: 'Customer',
     email: 'mike@aol.com',
-    password: bcrypt.hashSync('123', 10),
+    passwordHash: bcrypt.hashSync('123', 10),
     role: 'customer',
     isVerified: true,
     idDocumentHash: 'MIKE-HASH-001'
@@ -32,7 +32,7 @@ const users = [
     firstName: 'Tom',
     lastName: 'Customer',
     email: 'tom@aol.com',
-    password: bcrypt.hashSync('123', 10),
+    passwordHash: bcrypt.hashSync('123', 10),
     role: 'customer',
     isVerified: true,
     idDocumentHash: 'TOM-HASH-001'
@@ -41,7 +41,7 @@ const users = [
     firstName: 'Lee',
     lastName: 'Customer',
     email: 'lee@aol.com',
-    password: bcrypt.hashSync('123', 10),
+    passwordHash: bcrypt.hashSync('123', 10),
     role: 'customer',
     isVerified: true,
     idDocumentHash: 'LEE-HASH-001'
